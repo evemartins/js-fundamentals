db46a1b4e4de0f707b4421afc837ff9c676c0c37
Hericles Souza <hericles.souza@veredaeducacao.com>
2024-09-23 11:15:35 -0300

    feat: add new function to test if conditionals

---
+++ conditionals.js
@@ -0,0 +1,10 @@
+function alugarPatins(idade) {
+    if (idade >= 18) {
+        console.log("Pode fazer o aluguel");
+    } else {
+        console.log("Não poderá alugar");
+    }
+}
+
+alugarPatins(18);
+alugarPatins(17);
